86b25436db0e7810ee8dca5b46a6909b714a1648
rochafialvin <rochafi.dev@gmail.com>
2020-03-12 10:25:18 +0700

    Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ class App extends Component{
     render(){
         return(
             <div>
-                <h1>App Component</h1>
+                <Header/>
             </div>
         )
     }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,68 @@
 import React, { Component } from 'react'
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    NavbarText
+  } from 'reactstrap';
 
 class Header extends Component {
+
+    state = {
+        isOpen : false
+    }
+
+    toggle = () => this.setState({ isOpen : !this.state.isOpen })
+
     render() {
         return (
             <div>
-                <h1 className="text-center">Header Component</h1>
+                <Navbar color="light" light expand="md">
+                    <NavbarBrand href="/">reactstrap</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="mr-auto" navbar>
+                            <NavItem>
+                                <NavLink href="/components/">Components</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                            </NavItem>
+                            <UncontrolledDropdown nav inNavbar>
+                                <DropdownToggle nav caret>
+                                    Options
+                                </DropdownToggle>
+                                <DropdownMenu right>
+                                    <DropdownItem>
+                                        Option 1
+                                    </DropdownItem>
+                                    <DropdownItem>
+                                        Option 2
+                                    </DropdownItem>
+                                    <DropdownItem divider />
+                                    <DropdownItem>
+                                        Reset
+                                    </DropdownItem>
+                                </DropdownMenu>
+                            </UncontrolledDropdown>
+                        </Nav>
+                        
+                    </Collapse>
+                </Navbar>
             </div>
         )
     }
 }
 
 export default Header
+
+// const [isOpen, setIsOpen] = useState(false);
+// const togglex = () => setIsOpen(!isOpen);
